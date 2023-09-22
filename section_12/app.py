1bc68c57fec3d09bc94eedbda2af76b795e57144
tford-dev <terranceford513@gmail.com>
2023-09-22 14:12:14 -0400

    Added error handling to app.py

--- section_12/app.py
+++ section_12/app.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
 
-from pages.quotes_page import QuotesPage;
+from pages.quotes_page import QuotesPage, InvalidTagForAuthorError;
 
-chrome = webdriver.Chrome();
-chrome.get('https://quotes.toscrape.com/search.aspx');
-page = QuotesPage(chrome);
+try:
+    chrome = webdriver.Chrome();
+    chrome.get('https://quotes.toscrape.com/search.aspx');
+    page = QuotesPage(chrome);
 
-#for some reason I could not get a method created that would do what the next 4 lines of code does
-authors = page.get_authors()  # list
-print("Your options for authors to browse quotes from are:")
-for person in sorted(authors):
-    print(person)
+    #for some reason I could not get a method created that would do what the next 4 lines of code does
+    authors = page.get_authors()  # list
+    print("Your options for authors to browse quotes from are:")
+    for person in sorted(authors):
+        print(person)
 
-print(page.search_for_quotes())
+    print(page.search_for_quotes())
+except InvalidTagForAuthorError as e:
+    print(e);
+except Exception as e:
+    print(e);
+    print("An unknown error occured. Please try again.")
