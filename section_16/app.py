816bbd3a11703353429849784f66833a20b307ab
“tford-dev” <terranceford513@gmail.com>
2023-09-27 04:45:20 -0400

    Completed section 16

--- section_16/app.py
+++ section_16/app.py
@@ -3,9 +3,6 @@ from libs.openexchange import OpenExchangeClient
 
 APP_ID = '97894dc1419b40208bb130cde916673a'
 
-# ENDPOINT = 'https://openexchangerates.org/api/latest.json'
-# response = requests.get(f"{ENDPOINT}?app_id={APP_ID}")
-# exchange_rates = response.json()["rates"]
 client = OpenExchangeClient(APP_ID)
 
 usd_amount = 1000
